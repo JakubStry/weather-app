eb9991ae4960330f2e5353bddc902542faff70a3
Jakub <kuba2092001@interia.pl>
2025-05-26 16:57:52 +0200

    Set up listener and functions structure

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const weatherForm = document.querySelector('.weather-form');
 const cityInput = document.querySelector('.city-input');
 const card = document.querySelector('.card');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+async function getWeatherData(city) {}
+
+function displayWeatherInfo(data) {}
+
+function weatherEmoji(weatherId) {}
+
+function displayError(message) {}
