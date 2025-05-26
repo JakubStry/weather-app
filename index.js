89d0bc7ceb8f1b7c915c5eb8409808d0957fc060
Jakub <kuba2092001@interia.pl>
2025-05-26 20:10:05 +0200

    Add logic to displayWeatherInfo function and weatherEmoji function

--- index.js
+++ index.js
@@ -31,9 +31,64 @@ async function getWeatherData(city) {
   return data;
 }
 
-function displayWeatherInfo(data) {}
+function displayWeatherInfo(data) {
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ description, id }],
+  } = data;
 
-function weatherEmoji(weatherId) {}
+  card.textContent = '';
+  card.style.display = 'flex';
+
+  const cityDisplay = document.createElement('h1');
+  const tempDisplay = document.createElement('p');
+  const humidityDisplay = document.createElement('p');
+  const descriptionDisplay = document.createElement('p');
+  const weatherEmoji = document.createElement('p');
+
+  cityDisplay.textContent = city;
+  cityDisplay.classList.add('city-display');
+
+  tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
+  tempDisplay.classList.add('temp-display');
+
+  humidityDisplay.textContent = `Humidity: ${humidity}%`;
+  humidityDisplay.classList.add('humidity-display');
+
+  descriptionDisplay.textContent = description;
+  descriptionDisplay.classList.add('description-display');
+
+  weatherEmoji.textContent = getWeatherEmoji(id);
+  weatherEmoji.classList.add('weather-emoji');
+
+  card.appendChild(cityDisplay);
+  card.appendChild(tempDisplay);
+  card.appendChild(humidityDisplay);
+  card.appendChild(descriptionDisplay);
+  card.appendChild(weatherEmoji);
+}
+
+function getWeatherEmoji(weatherId) {
+  switch (true) {
+    case weatherId >= 200 && weatherId < 300:
+      return '⛈️';
+    case weatherId >= 300 && weatherId < 400:
+      return '🌦️';
+    case weatherId >= 500 && weatherId < 600:
+      return '🌧️';
+    case weatherId >= 600 && weatherId < 700:
+      return '❄️';
+    case weatherId >= 700 && weatherId < 800:
+      return '🌫️';
+    case weatherId === 800:
+      return '☀️';
+    case weatherId >= 801 && weatherId < 810:
+      return '☁️';
+    default:
+      return '❓';
+  }
+}
 
 function displayError(message) {
   const errorDisplay = document.createElement('p');
